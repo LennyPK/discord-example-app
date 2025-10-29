8613360e3a0133d1ca789e3b117cc9bb65c91057
Lenny <github@lennypk.dev>
2025-10-30 00:53:03 +1300

    :sparkles: feat: Add message scraper

--- database.js
+++ database.js
@@ -11,16 +11,34 @@ export async function saveMembers(members) {
 
     await prisma.user.upsert({
       where: { discordId: user.id },
-      update: { username: user.username, displayName: nick || user.global_name || user.username },
+      update: {
+        username: user.username,
+        guildName: nick || user.username,
+        globalName: user.global_name || user.username,
+      },
       create: {
         discordId: user.id,
         username: user.username,
-        displayName: user.global_name || user.username,
-        nickname: nick || null,
-        // Add seperate field for nickname
+        guildName: nick || user.username,
+        globalName: user.global_name || user.username,
       },
     })
   }
 
   return members.length
+}
+
+export async function findUserByMentionOrName(discordId, name) {
+  console.log("Searching for user with Discord ID:", discordId, "or name:", name)
+
+  if (discordId) {
+    return prisma.user.findUnique({ where: { discordId: discordId } })
+  }
+
+  // Otherwise, search by username, global name, or guild name
+  return prisma.user.findFirst({
+    where: {
+      OR: [{ globalName: name }, { username: name }, { guildName: name }],
+    },
+  })
 }
